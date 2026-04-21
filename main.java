0f8e83e3e5823f3b1f13157021b35ac9ed208f40
Johnlloyd Encierto <johnlloydencierto@gmail.com>
2026-04-21 03:19:32 +0000

    feat(main): adding main

---
+++ main.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class main{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        switch (sc) {
+            case value:
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
+}
